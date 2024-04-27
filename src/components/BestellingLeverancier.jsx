4e32bc1513d4359123bcc46826596dcf074a71f2
Mohisha Van Damme <mohisha.vandamme@student.hogent.be>
2024-04-27 18:43:51 +0200

    SDP2324VC2-293 BestellingRol aangepast naar de nieuwe table

--- src/components/BestellingLeverancier.jsx
+++ src/components/BestellingLeverancier.jsx
@@ -1,16 +1,6 @@
 import React, { useState } from "react";
-import Box from "@mui/material/Box";
-import Grid from "@mui/material/Grid";
-import Collapse from "@mui/material/Collapse";
-import IconButton from "@mui/material/IconButton";
-import Table from "@mui/material/Table";
-import TableBody from "@mui/material/TableBody";
 import TableCell from "@mui/material/TableCell";
-import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
-import Typography from "@mui/material/Typography";
-import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
-import KeyboardArrowUpIcon from "@mui/icons-material/KeyboardArrowUp";
 
 const BestellingLeverancier = ({ bestelling }) => {
   const [open, setOpen] = useState(false);
@@ -20,15 +10,6 @@ const BestellingLeverancier = ({ bestelling }) => {
   return (
     <>
       <TableRow sx={{ "& > *": { borderBottom: "unset" } }}>
-        <TableCell>
-          <IconButton
-            aria-label="expand row"
-            size="small"
-            onClick={() => setOpen(!open)}
-          >
-            {open ? <KeyboardArrowUpIcon /> : <KeyboardArrowDownIcon />}
-          </IconButton>
-        </TableCell>
         <TableCell component="th" scope="row" align="center">
           {DATUMGEPLAATST}
         </TableCell>
@@ -36,77 +17,6 @@ const BestellingLeverancier = ({ bestelling }) => {
         <TableCell align="center">{ORDERID}</TableCell>
         <TableCell align="center">{ORDERSTATUS}</TableCell>
         <TableCell align="center">{BETALINGSTATUS}</TableCell>
-      </TableRow>
-      <TableRow>
-        <TableCell style={{ paddingBottom: 0, paddingTop: 0 }} colSpan={6}>
-          <Collapse in={open} timeout="auto" unmountOnExit>
-          <Box sx={{ margin: 5 }}>
-              <Typography variant="h6" gutterBottom component="div">
-                <Grid 
-                  container
-                  spacing={3}
-                  sx={{ height: "100%", width: "100%" }}
-                >
-                  <Grid xs={11}>Gegevens</Grid>
-                  <Grid xs={3}>
-                    <div>Contactgegevens:</div>
-                  </Grid>
-                  <Grid xs={4}>
-                    <div>{klant.KLANT_EMAILADRES}</div>
-                    <div>{klant.TELEFOONNUMMER}</div>
-                  </Grid>
-                  <Grid xs={2}>
-                    <div>Totale bedrag:</div>
-                  </Grid>
-                  <Grid xs={3}>
-                    <div>prijs</div>
-                  </Grid>
-                  <Grid xs={3}>
-                    <div>Leveradres:</div>
-                  </Grid>
-                  <Grid xs={8}>
-                    <div>{klant.LAND}</div>
-                    <div>{klant.POSTCODE}</div>
-                    <div>{klant.STAD}</div>
-                    <div>{klant.STRAAT}</div>
-                    <div>{klant.STRAATNR}</div>
-                  </Grid>
-                  <Grid xs={3}>
-                    <div>Betalingsherinnering:</div>
-                  </Grid>
-                  <Grid xs={8}>
-                    <div>{HERINNERINGSDATUM}</div>
-                  </Grid>
-                </Grid>
-                Product
-              </Typography>
-              <Table size="small" aria-label="purchases">
-                <TableHead>
-                  <TableRow>
-                    <TableCell align="center">Naam</TableCell>
-                    <TableCell align="center">Aantal</TableCell>
-                    <TableCell align="center">In stock</TableCell>
-                    <TableCell align="center">Eeheidsprijs</TableCell>
-                    <TableCell align="center">Totale prijs</TableCell>
-                  </TableRow>
-                </TableHead>
-                {/* <TableBody>
-                    {b.PRODUCT.map((PRODUCTROW) => (
-                      <TableRow key={PRODUCTROW.NAAM}>
-                        <TableCell component="th" scope="row" align="right">
-                          {PRODUCTROW.NAAM}
-                        </TableCell>
-                        <TableCell align="center">{PRODUCTROW.AANTAL}</TableCell>
-                        <TableCell align="center">{PRODUCTROW.STOCK}</TableCell>
-                        <TableCell align="center">{PRODUCTROW.EENHEIDSPRIJS}</TableCell>
-                        <TableCell align="center">{PRODUCTROW.TOTALEPRIJS}</TableCell>
-                      </TableRow>
-                    ))}
-                  </TableBody> */}
-              </Table>
-            </Box>
-          </Collapse>
-        </TableCell>
       </TableRow>
     </>
   );
