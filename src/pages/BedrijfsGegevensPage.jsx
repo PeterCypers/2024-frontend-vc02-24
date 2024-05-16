5fab5c2492bdb2e5c4d7b12b7089f2a66aaeb82c
Mohisha Van Damme <mohisha.vandamme@student.hogent.be>
2024-05-16 15:14:18 +0200

    SDP2324VC2-309 Bedrijfsgegevens wijzigen alles zonder dat de update werkt

--- src/pages/BedrijfsGegevensPage.jsx
+++ src/pages/BedrijfsGegevensPage.jsx
@@ -1,11 +1,13 @@
-import React from 'react';
-import useSWR from "swr";
+import React, { useState } from 'react';
+import useSWR, { mutate } from "swr";
 import { useAuth } from "../contexts/Auth.context";
-import { getById } from '../api';
-import { Button, Link } from '@mui/material';
+import { getById, save, updateBedrijfgegevens } from '../api';
+import { Button, Dialog, DialogActions, DialogContent, DialogTitle, TextField } from '@mui/material';
 
 const BedrijfsGegevensPage = () => {
   const { gebruikerId, loading } = useAuth();
+  const [dialogOpen, setDialogOpen] = useState(false);
+
   if (loading) {
     return <div>Loading...</div>;
   }
@@ -16,11 +18,36 @@ const BedrijfsGegevensPage = () => {
     error,
   } = useSWR(`bedrijfsgegevens/${gebruikerId}`, getById);
 
+  const handleOpenEdit = () => {
+    setDialogOpen(true);
+  };
+
+  const handleCloseDialog = () => {
+    setDialogOpen(false);
+  };
+
+  const handleSaveData = async (data) => {
+    await updateBedrijfgegevens(gebruikerId, {
+      NAAM: data.NAAM, 
+      BTWNR: data.BTWNR, 
+      EMAILADRES: data.EMAILADRES, 
+      LOGO: data.LOGO, 
+      REKENINGNUMMER: data.REKENINGNUMMER, 
+      SECTOR: data.SECTOR, 
+      TELEFOONNUMMER: data.TELEFOONNUMMER, 
+      LAND: data.LAND, 
+      POSTCODE: data.POSTCODE, 
+      STAD: data.STAD, 
+      STRAAT: data.STRAAT, 
+      STRAATNR: data.STRAATNR,
+    });
+    mutate();
+  };
+
   if(isLoading){
     return <div>Loading...</div>
   }
 
-  console.log(bedrijf);
   return (
     <div className="grid grid-cols-2 gap-6 w-full m-5">
       <div className="ml-5 mt-3">
@@ -46,9 +73,159 @@ const BedrijfsGegevensPage = () => {
         <img src={bedrijf.LOGO} alt="Image" />
       </div>
       <div className='ml-5'>
-        <Button variant="contained" href='/profiel/bedrijfsgegevens/wijzigen'>Gegevens wijzigen</Button>
+        <Button variant="contained" onClick={handleOpenEdit}>Gegevens wijzigen</Button>
       </div>
+      <UpdateDialog open={dialogOpen} handleClose={handleCloseDialog} initialData={bedrijf} onSave={handleSaveData} />
     </div>
+  );
+};
+
+const UpdateDialog = ({ open, handleClose, initialData, onSave }) => {
+  const [formData, setFormData] = useState(initialData);
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setFormData(prev => ({ ...prev, [name]: value }));
+  };
+
+  const handleSave = () => {
+    onSave(formData);
+    handleClose();
+  };
+
+  return (
+    <Dialog maxWidth={'lg'} open={open} onClose={handleClose}>
+      <DialogTitle>Bedrijf gegevens wijzigen</DialogTitle>
+      <DialogContent>
+        <div className='grid grid-cols-2 gap-3 w-fit'>
+        <TextField
+          margin="dense"
+          label="Bedrijfsnaam"
+          type="text"
+          fullWidth
+          variant="outlined"
+          name="NAAM"
+          value={formData.NAAM}
+          onChange={handleChange}
+        />
+        <TextField
+          margin="dense"
+          label="Sector"
+          type="text"
+          fullWidth
+          variant="outlined"
+          name="SECTOR"
+          value={formData.SECTOR}
+          onChange={handleChange}
+        />
+        <TextField className='col-span-2'
+          margin="dense"
+          label="Logo"
+          type="url"
+          fullWidth
+          variant="outlined"
+          name="LOGO"
+          value={formData.LOGO}
+          onChange={handleChange}
+        />
+        <TextField
+          margin="dense"
+          label="E-mailadres"
+          type="email"
+          fullWidth
+          variant="outlined"
+          name="EMAILADRES"
+          value={formData.EMAILADRES}
+          onChange={handleChange}
+        />
+        <TextField
+          margin="dense"
+          label="Telefoonnummer"
+          type="tel"
+          fullWidth
+          variant="outlined"
+          name="TELEFOONNUMMER"
+          value={formData.TELEFOONNUMMER}
+          onChange={handleChange}
+        />
+        <TextField
+          margin="dense"
+          label="Rekeningnummer"
+          type="text"
+          fullWidth
+          variant="outlined"
+          name="REKENINGNUMMER"
+          value={formData.REKENINGNUMMER}
+          onChange={handleChange}
+        />
+        <TextField
+          margin="dense"
+          label="BTW-nummer"
+          type="text"
+          fullWidth
+          variant="outlined"
+          name="BTWNR"
+          value={formData.BTWNR}
+          onChange={handleChange}
+        />
+        <TextField className='col-span-2'
+          margin="dense"
+          label="Straat"
+          type="text"
+          fullWidth
+          variant="outlined"
+          name="STRAAT"
+          value={formData.STRAAT}
+          onChange={handleChange}
+        />
+        <TextField
+          margin="dense"
+          label="Volgnummer"
+          type="number"
+          fullWidth
+          variant="outlined"
+          name="STRAATNR"
+          value={formData.STRAATNR}
+          onChange={handleChange}
+        />
+        <TextField
+          margin="dense"
+          label="Postcode"
+          type="number"
+          fullWidth
+          variant="outlined"
+          name="POSTCODE"
+          value={formData.POSTCODE}
+          onChange={handleChange}
+        />
+        <TextField
+          margin="dense"
+          label="Stad"
+          type="text"
+          fullWidth
+          variant="outlined"
+          name="STAD"
+          value={formData.STAD}
+          onChange={handleChange}
+        />
+        <TextField
+          margin="dense"
+          label="Land"
+          type="text"
+          fullWidth
+          variant="outlined"
+          name="LAND"
+          value={formData.LAND}
+          onChange={handleChange}
+        />
+        </div>
+        
+      </DialogContent>
+      <DialogActions>
+        <Button onClick={handleClose}>Annuleren</Button>
+        <Button onClick={handleSave} color="primary">Opslaan</Button>
+      </DialogActions>
+    </Dialog>
   );
 };
 
